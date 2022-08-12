ba1e99f1350966b4c06fab87b0b53570c2d967d0
Jenkin <zhaojiancun@yunquna.com>
2022-08-12 19:08:31 +0800

    feat init

---
+++ TEST/Controllers/ShouDongAPI.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TEST.Controllers
+{
+    [Route("api/shoudongapi")]
+    public class ShouDongAPIController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get() => new String[] { "value1", "value2" };
+    }
+}
+
