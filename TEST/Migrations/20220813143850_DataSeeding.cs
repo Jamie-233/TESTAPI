c3c0abff008889799139a9462777ee4cee3ab386
Jenkin <zhaojiancun@yunquna.com>
2022-08-14 00:45:49 +0800

    chore: generate migrations

---
+++ TEST/Migrations/20220813143850_DataSeeding.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TEST.Migrations
+{
+    public partial class DataSeeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "TouristRoutes",
+                columns: new[] { "Id", "CreateTime", "DepartureTime", "Description", "DiscountPresent", "Feature", "Fees", "Notes", "OriginalPrice", "Title", "UpdateTime" },
+                values: new object[] { new Guid("a6612a48-7b7d-4fd2-a4f3-266ab17dc649"), new DateTime(2022, 8, 13, 14, 38, 50, 657, DateTimeKind.Utc).AddTicks(1290), null, "description", null, null, null, null, 0m, "test title", null });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "TouristRoutes",
+                keyColumn: "Id",
+                keyValue: new Guid("a6612a48-7b7d-4fd2-a4f3-266ab17dc649"));
+        }
+    }
+}
--- TEST/Migrations/20220813163202_DataSeeding2.Designer.cs
+++ TEST/Migrations/20220813163202_DataSeeding2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TEST.Database;
 
@@ -11,9 +12,10 @@ using TEST.Database;
 namespace TEST.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220813163202_DataSeeding2")]
+    partial class DataSeeding2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
