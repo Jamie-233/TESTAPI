c2802e43a2a00d2d57e8bbeb18f54dba9a4eba72
Jenkin <zhaojiancun@yunquna.com>
2022-08-13 18:03:15 +0800

    chore: update dotnet core version

---
+++ TEST/Migrations/20220813095524_initialMigration.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TEST.Migrations
+{
+    public partial class initialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TouristRoutes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Fees = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Notes = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Feature = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(1500)", maxLength: 1500, nullable: false),
+                    OriginalPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    DiscountPresent = table.Column<double>(type: "float", nullable: true),
+                    CreateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdateTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DepartureTime = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TouristRoutes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TouristRoutePirctures",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Url = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    TouristRouteId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TouristRoutePirctures", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TouristRoutePirctures_TouristRoutes_TouristRouteId",
+                        column: x => x.TouristRouteId,
+                        principalTable: "TouristRoutes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TouristRoutePirctures_TouristRouteId",
+                table: "TouristRoutePirctures",
+                column: "TouristRouteId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TouristRoutePirctures");
+
+            migrationBuilder.DropTable(
+                name: "TouristRoutes");
+        }
+    }
+}
